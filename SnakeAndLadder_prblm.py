961d99970229cc1c43f947f5cd655bd0e6eb2551
tanyamaheshwari-05 <tanya47887@gmail.com>
2026-03-10 22:17:42 +0530

    feat:Add code of chooosing option for snake,Ladder and no move

--- SnakeAndLadder_prblm.py
+++ SnakeAndLadder_prblm.py
@@ -5,8 +5,24 @@ print("Welcome to Sanke and Ladder game")
 position1 = 0
 print("Start Position of Player 1: ",position1)
 
+
 def roll_dice():
     dice=random.randint(1, 6)
     return dice
 
 
+def option_choose():
+    options=["No play","Snake","Ladder"]
+    return random.choice(options)
+
+dice= roll_dice()
+option=option_choose()
+
+if(option=="No play"):
+    print("Player stays at the same position : ",position1)
+
+elif (option=="Ladder"):
+    position1+=dice
+
+elif(option=="Snake"):
+    position-=dice
