f329a3e535552a468ea8f994a6f50739d7246fa6
tanyamaheshwari-05 <tanya47887@gmail.com>
2026-03-10 22:34:24 +0530

    feat:Add code of count of how many time dice rolls and position update after each roll

--- SnakeAndLadder_prblm.py
+++ SnakeAndLadder_prblm.py
@@ -5,6 +5,7 @@ print("Welcome to Sanke and Ladder game")
 position1 = 0
 print("Start Position of Player 1: ",position1)
 
+count=0
 
 def roll_dice():
     dice=random.randint(1, 6)
@@ -18,6 +19,7 @@ def option_choose():
 while position1<100:
     dice= roll_dice()
     option=option_choose()
+    count+=1
 
     if(option=="No play"):
         print("Player stays at the same position : ",position1)
@@ -25,9 +27,11 @@ while position1<100:
     elif (option=="Ladder"):
         if position1 + dice <= 100 :
             position1+=dice
+        print("Player move forward:", position1)
 
     elif(option=="Snake"):
         position1-=dice
         if(position1<0):
             position1=0
-
+        print("Snake Bite! Position of player:", position1)
+print("Total dice rolled:", count)
