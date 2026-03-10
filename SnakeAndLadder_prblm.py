0e22ad87acd1132515ea083b0af3295e60e99f29
tanyamaheshwari-05 <tanya47887@gmail.com>
2026-03-10 22:11:25 +0530

    feat:Add code of random dice roll

--- SnakeAndLadder_prblm.py
+++ SnakeAndLadder_prblm.py
@@ -1,4 +1,12 @@
+import random
+
 print("Welcome to Sanke and Ladder game")
 
 position1 = 0
 print("Start Position of Player 1: ",position1)
+
+def roll_dice():
+    dice=random.randint(1, 6)
+    return dice
+
+
